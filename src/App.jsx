362d206b7ec28ca6ba4bd528c3db7e4a2996c4d5
Cody Kremer <ckremer99@outlook.com>
2024-08-06 13:41:17 -0700

    added form

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import TrackForm from './components/Trackform'
 
 const App = () => {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <Routes>
+        <Route path='/add-track' element={<TrackForm formType={"Create"}/>}/>
+        <Route path='/edit-track/:trackId' element={<TrackForm formType={"Edit"}/>}/>
+      </Routes>
+    </>
+  )
 };
 
 export default App;
---
+++ src/components/Trackform.jsx
@@ -0,0 +1,44 @@
+
+import { useState } from 'react'
+
+const TrackForm = (props) => {
+    const [formData, setFormData] = useState({title: '', artist: ''});
+
+
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        setFormData({title: '', artist: ''})
+      };
+    
+
+    const handleChange = (evt) => {
+        setFormData({ ...formData, [evt.target.name]: evt.target.value });
+      };
+
+    return (
+        <>
+            <h1>Create/Edit Track</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="title">Title: </label>
+                <input 
+                    required
+                    type="text"
+                    name="title"
+                    id="title"
+                    value={formData.title}
+                    onChange={handleChange} />
+                <label htmlFor="artist">Artist: </label>
+                <input 
+                    required
+                    type="text"
+                    name="artist"
+                    id="artist"
+                    value={formData.artist}
+                    onChange={handleChange} />
+                <button>Submit</button>
+            </form>
+        </>
+    )
+}
+
+export default TrackForm
